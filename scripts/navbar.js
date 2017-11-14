a94a1ded447b1aefd217ed3ddd9b9eec9c751c8e
Benjamin Mayer <lolgame99@gmail.com>
2017-11-14 19:27:31 +0100

    FIXED Navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,30 +1,30 @@
 $(function() {
     $("body").on('click', '.homeA', function(event) {
-        $(".home").html('<a class="nav-link homeA">Home <span class="sr-only">(current)</span></a>');
-        $(".temp").html('<a class="nav-link tempA">Temperatur</a>');
-        $(".humid").html('<a class="nav-link humidA">Luftfeuchtigkeit></a>');
-        $(".light").html('<a class="nav-link lightA">Helligkeit</a>');
+        $(".home").addClass('active');
+        $(".temp").removeClass('active');
+        $(".humid").removeClass('active');
+        $(".light").removeClass('active');
     });
 
     $("body").on('click', '.tempA', function(event) {
-        $(".home").html('<a class="nav-link homeA">Home</a>');
-        $(".temp").html('<a class="nav-link tempA">Temperatur <span class="sr-only">(current)</span></a>');
-        $(".humid").html('<a class="nav-link humidA">Luftfeuchtigkeit</a>');
-        $(".light").html('<a class="nav-link lightA">Helligkeit</a>');
+        $(".home").removeClass('active');
+        $(".temp").addClass('active');
+        $(".humid").removeClass('active');
+        $(".light").removeClass('active');
     });
 
     $("body").on('click', '.humidA', function(event) {
-        $(".home").html('<a class="nav-link homeA">Home</a>');
-        $(".temp").html('<a class="nav-link tempA">Temperatur</a>');
-        $(".humid").html('<a class="nav-link humidA">Luftfeuchtigkeit <span class="sr-only">(current)</span></a>');
-        $(".light").html('<a class="nav-link lightA">Helligkeit</a>');
+        $(".home").removeClass('active');
+        $(".temp").removeClass('active');
+        $(".humid").addClass('active');
+        $(".light").removeClass('active');
     });
 
     $("body").on('click', '.lightA', function(event) {
-        $(".home").html('<a class="nav-link homeA">Home</a>');
-        $(".temp").html('<a class="nav-link tempA">Temperatur</a>');
-        $(".humid").html('<a class="nav-link humidA">Luftfeuchtigkeit</a>');
-        $(".light").html('<a class="nav-link lightA">Helligkeit <span class="sr-only">(current)</span></a>');
+        $(".home").removeClass('active');
+        $(".temp").removeClass('active');
+        $(".humid").addClass('active');
+        $(".light").removeClass('active');
     });
 
 });
