480bdc5fddc4a6ac68e75e91de30512875076021
lolgame99 <lolgame99@gmail.com>
2017-11-24 17:00:29 +0100

    added light chart script

--- scripts/lightChart.js
+++ scripts/lightChart.js
@@ -1,13 +1,47 @@
 $(function() {
+    var options = {};
     $.ajax({
         method: "GET",
         url: "http://vd9ksd.myvserver.online/lora/values?limit=10",
         success: function(data) {
-            var output = "";
-            var left = true;
-            var test = 0;
+            options = {
+                title: {
+                    text: "Helligkeit"
+                },
+                animationEnabled: true,
+                axisY: {
+                    includeZero: false,
+                    maximum: 10,
+                    valueFormatString: "#0"
+                },
+                axisX: {
+                    title: "Zeitpunkt"
+                },
+                toolTip: {
+                    shared: true,
+                    content: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span> {y}"
+                },
+
+                data: [{
+                    type: "splineArea",
+                    showInLegend: true,
+                    name: "Helligkeit",
+                    dataPoints: [{
+                        x: 1,
+                        y: 3000
+                    }]
+                }]
+            };
+
             $.each(data, function(id, obj) {
-                console.log(test);
+                var coords = {};
+                var str = obj.payload_fields.light;
+                var lightVal = str.substr(str.indexOf("(") + 1, str.indexOf(","));
+                str = obj.metadata.time;
+                coords.y = lightVal;
+                coords.x = str.substr(str.indexOf("T") + 1, str.indexOf("T") + 6);
+
+                options.data.dataPoints.push(coords);
 
 
             });
@@ -18,71 +52,7 @@ $(function() {
         }
     });
     //Better to construct options first and then pass it as a parameter
-    var options = {
-        title: {
-            text: "Helligkeit"
-        },
-        animationEnabled: true,
-        axisY: {
-            includeZero: false,
-            maximum: 110000,
-            valueFormatString: "#0,."
-        },
-        axisX: {
-            title: "Zeitpunkt"
-        },
-        toolTip: {
-            shared: true,
-            content: "<span style='\"'color: {color};'\"'><strong>{name}</strong></span> {y}"
-        },
 
-        data: [{
-            type: "splineArea",
-            showInLegend: true,
-            name: "Helligkeit",
-            dataPoints: [{
-                    x: 1,
-                    y: 3000
-                },
-                {
-                    x: 2,
-                    y: 7000
-                },
-                {
-                    x: 3,
-                    y: 10000
-                },
-                {
-                    x: 4,
-                    y: 14000
-                },
-                {
-                    x: 5,
-                    y: 23000
-                },
-                {
-                    x: 6,
-                    y: 31000
-                },
-                {
-                    x: 7,
-                    y: 42000
-                },
-                {
-                    x: 8,
-                    y: 56000
-                },
-                {
-                    x: 9,
-                    y: 64000
-                },
-                {
-                    x: 10,
-                    y: 81000
-                }
-            ]
-        }]
-    };
     $(".chartContainer").CanvasJSChart(options);
 
 });
